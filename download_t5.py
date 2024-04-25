357d93a6e38af479756e1a14415e5f0982475772
duanjunwen <935724073@qq.com>
2024-04-25 06:25:31 +0000

    [feat] never merge this PR;

---
+++ download_t5.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ['HF_ENDPOINT'] = 'https://hf-mirror.com'
+os.system('huggingface-cli download --resume-download DeepFloyd/t5-v1_1-xxl --local-dir /home/duanjunwen/LCProj/Open-Sora/pretrained_models/t5_ckpts/t5-v1_1-xxl --local-dir-use-symlinks False')
+# Open-Sora/pretrained_models/t5_ckpts/t5-v1_1-xxl
